05a84e377a883db59b9a3ea678d98908e0e24c42
TitusKI <kidumazie@gmail.com>
2024-12-07 02:54:44 +0300

    Add UserModel for user data representation and conversion methods

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,59 @@
+import 'package:smart_crm_app/features/auth/domain/entities/signup_entity.dart';
+
+class UserModel {
+  final String email;
+  final String password;
+  final String confirmPassword;
+  final String name;
+  final String? profileImage;
+  final String? role;
+  const UserModel({
+    required this.email,
+    required this.password,
+    required this.confirmPassword,
+    required this.name,
+    this.profileImage,
+    this.role,
+  });
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'password': password,
+      'confirmPassword': confirmPassword,
+      'name': name,
+      'profileImage': profileImage,
+      'role': role,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      email: map['email'],
+      password: map['password'],
+      confirmPassword: map['confirmPassword'],
+      name: map['name'],
+      profileImage: map['profileImage'],
+      role: map['role'],
+    );
+  }
+  factory UserModel.fromEntity(UserEntity entity) {
+    return UserModel(
+      email: entity.email,
+      password: entity.password,
+      confirmPassword: entity.confirmPassword,
+      name: entity.name,
+      profileImage: entity.profileImage,
+      role: entity.role,
+    );
+  }
+  UserEntity toEntity() {
+    return UserEntity(
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      name: name,
+      profileImage: profileImage,
+      role: role,
+    );
+  }
+}
