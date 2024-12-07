62bd6989484e22d68b64a9899c6be3df165d4aeb
TitusKI <kidumazie@gmail.com>
2024-12-07 03:01:20 +0300

    Add USER_ID_KEY constant and create exports file for service and use case access

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -2,4 +2,5 @@ class AppConstant {
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_time';
   static const String USER_TOKEN = 'user_token';
   static const String BASE_URL = "http://192.168.43.184:5001/api/v1";
+  static const String USER_ID_KEY = 'user_id';
 }
