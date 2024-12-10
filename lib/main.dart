630415c439a3f37791c8de641aff0ade747aa703
TitusKI <kidumazie@gmail.com>
2024-12-10 04:19:18 +0300

    Refactor main app structure to use MultiBlocProvider and integrate ThemeCubit for dynamic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_crm_app/config/routes/pages.dart';
+import 'package:smart_crm_app/features/auth/presentation/bloc/theme_cubit.dart';
 import 'package:smart_crm_app/injection_container.dart';
 
 Future<void> main() async {
   await initializeInjection();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AppPages appPages = AppPages();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-          body: Container(child: const Center(child: Text('Smart CRM APP')))),
+    return MultiBlocProvider(
+      providers: [
+        ...appPages.allBlocProvider(context),
+        BlocProvider(create: (context) => ThemeCubit()),
+      ],
+      child: ScreenUtilInit(
+          builder: (context, child) => BlocBuilder<ThemeCubit, ThemeData>(
+                builder: (context, theme) {
+                  return MaterialApp(
+                    theme: theme,
+                    debugShowCheckedModeBanner: false,
+                    onGenerateRoute: appPages.generateRouteSettings,
+                  );
+                },
+              )),
     );
   }
 }
