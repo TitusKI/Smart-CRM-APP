03cee5644e29ac06ea9458e53f9286f92e4219c2
TitusKI <kidumazie@gmail.com>
2024-12-10 04:19:00 +0300

    Add dependency injection for user management and dashboard cubit

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smart_crm_app/features/auth/domain/usecases/delete_user.dart';
+import 'package:smart_crm_app/features/auth/presentation/bloc/verification/verification_bloc.dart';
+
+import 'package:smart_crm_app/features/common/bottom_navigation/bottom_navigation_bloc.dart';
+
 import 'core/constants/exports.dart';
 import 'features/auth/data/repository/auth_repository_impl.dart';
 import 'features/auth/domain/repository/auth_repository.dart';
@@ -6,13 +13,18 @@ import 'features/auth/domain/usecases/logout.dart';
 import 'features/auth/domain/usecases/signup.dart';
 import 'package:get_it/get_it.dart';
 
+import 'features/auth/presentation/bloc/sign_in/sign_in_bloc.dart';
+import 'features/auth/presentation/bloc/sign_up/sign_up_bloc.dart';
 import 'features/contacts/domain/usecases/add_contact.dart';
 import 'features/contacts/domain/usecases/get_all_contacts.dart';
 import 'features/contacts/domain/usecases/update_contact.dart';
+import 'features/dashboard/presentation/bloc/dashboard_cubit.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initializeInjection() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // local storage
   sl.registerSingleton<StorageServices>(await StorageServices().init());
 
@@ -37,9 +49,9 @@ Future<void> initializeInjection() async {
   sl.registerSingleton<LogoutUsecase>(LogoutUsecase());
   sl.registerSingleton<DeleteAccountUsecase>(DeleteAccountUsecase());
   sl.registerSingleton<CreateUserUsecase>(CreateUserUsecase());
-  sl.registerSingleton<DeleteContactUsecase>(DeleteContactUsecase());
   sl.registerSingleton<GetAllUsersUsecase>(GetAllUsersUsecase());
   sl.registerSingleton<GetUserByIdUsecase>(GetUserByIdUsecase());
+  sl.registerSingleton<DeleteUserUsecase>(DeleteUserUsecase());
 
 // Contact usecases
   sl.registerSingleton<AddContactUsecase>(AddContactUsecase());
@@ -62,4 +74,12 @@ Future<void> initializeInjection() async {
   sl.registerSingleton<LeadApprovalQueueUsecase>(LeadApprovalQueueUsecase());
   sl.registerSingleton<RejectLeadUsecase>(RejectLeadUsecase());
   sl.registerSingleton<UpdateLeadUsecase>(UpdateLeadUsecase());
+
+  // blocs
+  sl.registerFactory<SignInBloc>(() => SignInBloc());
+  sl.registerFactory<SignUpBloc>(() => SignUpBloc());
+  sl.registerFactory<BottomNavigationBloc>(() => BottomNavigationBloc());
+  sl.registerFactory<VerificationBloc>(() => VerificationBloc());
+  sl.registerFactory<DashboardCubit>(
+      () => DashboardCubit()..fetchDashboardData());
 }
